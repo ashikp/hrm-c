1fbeac18ca862fde4c756de8dfff14ecc5a32bb3
Ehfaz Mahee <mahee504@gmail.com>
2021-02-11 03:07:33 +0600

    menu update

--- HRM/menu.c
+++ HRM/menu.c
@@ -95,9 +95,18 @@ bannerdesign("Employee Menu");
     printf("2. Hire Employee");
 
     getcod(20,7);
-    printf("3. << Go Back");
+    printf("4. Bank and Tax");
+
+    getcod(20,8);
+    printf("5. Salary administration");
+
+    getcod(20,9);
+    printf("6. << Go Back");
 
     getcod(20,10);
+    printf("3. << Go Back");
+
+    getcod(20,11);
     printf("Select Menu:     ");
     scanf("%i", &select);
 
@@ -120,6 +129,7 @@ bannerdesign("Employee Menu");
 
 }
 void admin_facility_menu(){
+    system("cls");
 bannerdesign("Facility Menu");
     int select;
 
@@ -154,8 +164,97 @@ bannerdesign("Facility Menu");
         }
 
 }
-void admin_financial_menu(){}
-void admin_time_menu(){}
+void admin_financial_menu(){
+system("cls");
+bannerdesign("Financial Menu");
+    int select;
+
+    getcod(20,4);
+    printf("1. Payroll");
+
+    getcod(20,5);
+    printf("2. Bank and Taxes");
+
+
+    getcod(20,6);
+    printf("3. Salary Administration");
+
+
+    getcod(20,7);
+    printf("4. Rewards");
+
+
+    getcod(20,8);
+    printf("5. << Go Back");
+
+    getcod(20,10);
+    printf("Select Menu:     ");
+    scanf("%i", &select);
+
+        switch(select){
+        case 1:
+            admin_fi_payroll();
+            break;
+        case 2:
+            admin_fi_bank_and_taxes();
+            break;
+        case 3:
+            admin_fi_salary_administration();
+            break;
+        case 4:
+            admin_fi_rewards();
+            break;
+        case 5:
+            Administrator_Menu();
+            break;
+        default:
+            printf("Wrong Input \n");
+            printf("Press any key to Try Again \n");
+            getch();
+            admin_user_menu();
+        }
+
+}
+void admin_time_menu(){
+system("cls");
+bannerdesign("Time Menu");
+    int select;
+
+    getcod(20,5);
+    printf("1. Clock In or Out");
+
+    getcod(20,6);
+    printf("2. Time Tracking");
+
+    getcod(20,7);
+    printf("3. Attendance Management");
+
+    getcod(20,8);
+    printf("4. << Go Back");
+
+    getcod(20,10);
+    printf("Select Menu:     ");
+    scanf("%i", &select);
+
+        switch(select){
+        case 1:
+            admin_t_clock_in_or_out();
+            break;
+        case 2:
+            admin_t_time_tracking();
+            break;
+        case 3:
+            admin_t_attendance_management();
+            break;
+        case 4:
+            Administrator_Menu();
+        default:
+            printf("Wrong Input \n");
+            printf("Press any key to Try Again \n");
+            getch();
+            admin_user_menu();
+        }
+}
 
 void admin_user_menu(){
 system("cls");
@@ -193,7 +292,41 @@ bannerdesign("User Menu");
         }
 
 }
-void admin_project_menu(){}
+void admin_project_menu(){
+system("cls");
+bannerdesign("Project Menu");
+    int select;
+
+    getcod(20,5);
+    printf("1. Projects");
+
+    getcod(20,6);
+    printf("2. Client Management");
+
+    getcod(20,7);
+    printf("3. << Go Back");
+
+    getcod(20,10);
+    printf("Select Menu:     ");
+    scanf("%i", &select);
+
+        switch(select){
+        case 1:
+            admin_p_projects();
+            break;
+        case 2:
+            admin_p_client_management();
+            break;
+        case 3:
+            Administrator_Menu();
+            break;
+        default:
+            printf("Wrong Input \n");
+            printf("Press any key to Try Again \n");
+            getch();
+            admin_user_menu();
+        }
+}
 void admin_reports(){}
 
 void admin_user_management(){
@@ -251,3 +384,12 @@ void admin_e_information_database(){}
 void admin_e_hire_employee(){}
 void admin_f_insurance(){}
 void admin_f_over_time(){}
+void admin_fi_payroll(){}
+void admin_fi_bank_and_taxes(){}
+void admin_fi_salary_administration(){}
+void admin_fi_rewards(){}
+void admin_t_clock_in_or_out(){}
+void admin_t_time_tracking(){}
+void admin_t_attendance_management(){}
+void admin_p_projects(){}
+void admin_p_client_management(){}
