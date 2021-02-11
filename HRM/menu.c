5a9843d9e33124392e4cdc56547303ef4311c685
Ehfaz Mahee <mahee504@gmail.com>
2021-02-12 00:20:02 +0600

    Main Update

--- HRM/menu.c
+++ HRM/menu.c
@@ -134,13 +134,16 @@ bannerdesign("Facility Menu");
     int select;
 
     getcod(20,5);
-    printf("1. Insurance");
+    printf("1. Facility Administration");
 
     getcod(20,6);
-    printf("2.Over Time");
+    printf("2.Insurance");
 
     getcod(20,7);
-    printf("3. << Go Back");
+    printf("3. Over Time");
+
+    getcod(20,8);
+    printf("4. << Go Back");
 
     getcod(20,10);
     printf("Select Menu:     ");
@@ -148,12 +151,15 @@ bannerdesign("Facility Menu");
 
         switch(select){
         case 1:
-            admin_f_insurance();
+            admin_f_facility_administration();
             break;
         case 2:
-            admin_f_over_time();
+            admin_f_insurance();
             break;
         case 3:
+            admin_f_over_time();
+            break;
+        case 4:
             Administrator_Menu();
             break;
         default:
@@ -380,16 +386,453 @@ bannerdesign("User Management");
 
 }
 void admin_role_control_policy(){}
-void admin_e_information_database(){}
+void admin_e_information_database(){
+system("cls");
+bannerdesign("Information Database");
+    int select;
+
+    getcod(20,5);
+    printf("1. Add Employee");
+
+    getcod(20,6);
+    printf("2. Edit Employee");
+
+    getcod(20,7);
+    printf("3. Delete Employee");
+
+    getcod(20,8);
+    printf("4. Job History Overview");
+
+    getcod(20,9);
+    printf("5. << Go Back");
+
+    getcod(20,10);
+    printf("Select Menu:     ");
+    scanf("%i", &select);
+
+        switch(select){
+        case 1:
+            admin_add_employee();
+            break;
+        case 2:
+            admin_edit_employee();
+            break;
+        case 3:
+            admin_delete_employee();
+            break;
+        case 4:
+            admin_job_history_overview();
+            break;
+        case 5:
+            Administrator_Menu();
+            break;
+        default:
+            printf("Wrong Input \n");
+            printf("Press any key to Try Again \n");
+            getch();
+            admin_user_menu();
+        }
+}
 void admin_e_hire_employee(){}
-void admin_f_insurance(){}
-void admin_f_over_time(){}
-void admin_fi_payroll(){}
-void admin_fi_bank_and_taxes(){}
+void admin_f_facility_administration(){
+system("cls");
+bannerdesign("Information Database");
+    int select;
+
+    getcod(20,5);
+    printf("1. Add Facility");
+
+    getcod(20,6);
+    printf("2. View Facility");
+
+    getcod(20,7);
+    printf("3. Assign Facility");
+
+    getcod(20,8);
+    printf("4. Employee With Facility");
+
+    getcod(20,9);
+    printf("5. << Go Back");
+
+    getcod(20,10);
+    printf("Select Menu:     ");
+    scanf("%i", &select);
+
+        switch(select){
+        case 1:
+            admin_add_facility();
+            break;
+        case 2:
+            admin_view_facility();
+            break;
+        case 3:
+            admin_assign_facility();
+            break;
+        case 4:
+            admin_employee_with_facility();
+            break;
+        case 5:
+            Administrator_Menu();
+            break;
+        default:
+            printf("Wrong Input \n");
+            printf("Press any key to Try Again \n");
+            getch();
+            admin_user_menu();
+        }
+
+}
+void admin_f_insurance(){
+system("cls");
+bannerdesign("Information Database");
+    int select;
+
+    getcod(20,5);
+    printf("1. Health Insurance");
+
+    getcod(20,6);
+    printf("2. Life Insurance");
+
+    getcod(20,7);
+    printf("3. << Go Back");
+
+    getcod(20,10);
+    printf("Select Menu:     ");
+    scanf("%i", &select);
+
+        switch(select){
+        case 1:
+            admin_health_insurance();
+            break;
+        case 2:
+            admin_life_insurance();
+            break;
+        case 3:
+            Administrator_Menu();
+            break;
+        default:
+            printf("Wrong Input \n");
+            printf("Press any key to Try Again \n");
+            getch();
+            admin_user_menu();
+        }
+
+}
+void admin_f_over_time(){
+system("cls");
+bannerdesign("Over Time");
+    int select;
+
+    getcod(20,5);
+    printf("1. Employee Overtime");
+
+    getcod(20,6);
+    printf("2. Overtime Facility");
+
+    getcod(20,7);
+    printf("3. Overtime Extra Benefit");
+
+    getcod(20,8);
+    printf("4. << Go Back");
+
+    getcod(20,10);
+    printf("Select Menu:     ");
+    scanf("%i", &select);
+
+        switch(select){
+        case 1:
+            admin_employee_overtime();
+            break;
+        case 2:
+            admin_overtime_facility();
+            break;
+        case 3:
+            admin_overtime_extra_benefit();
+            break;
+        case 4:
+            admin_employee_with_facility();
+            break;
+        case 5:
+            Administrator_Menu();
+            break;
+        default:
+            printf("Wrong Input \n");
+            printf("Press any key to Try Again \n");
+            getch();
+            admin_user_menu();
+        }
+
+}
+void admin_fi_payroll(){
+system("cls");
+bannerdesign("Payroll");
+    int select;
+
+    getcod(20,5);
+    printf("1. Add Pay Order");
+
+    getcod(20,6);
+    printf("2. Release Pay Order");
+
+    getcod(20,7);
+    printf("3. Expense");
+
+    getcod(20,8);
+    printf("4. Provident  Fund");
+
+    getcod(20,9);
+    printf("5. << Go Back");
+
+    getcod(20,10);
+    printf("Select Menu:     ");
+    scanf("%i", &select);
+
+        switch(select){
+        case 1:
+            admin_add_pay_order();
+            break;
+        case 2:
+            admin_release_pay_order();
+            break;
+        case 3:
+            admin_expense();
+            break;
+        case 4:
+            admin_provident_fund();
+            break;
+        case 5:
+            Administrator_Menu();
+            break;
+        default:
+            printf("Wrong Input \n");
+            printf("Press any key to Try Again \n");
+            getch();
+            admin_user_menu();
+        }
+
+}
+void admin_add_employee_bank_and_tax_details(){}
+void admin_yearly_tax_issues(){}
+void admin_fi_bank_and_taxes(){
+system("cls");
+bannerdesign("Information Database");
+    int select;
+
+    getcod(20,5);
+    printf("1. Add Employee Bank And Tax Details");
+
+    getcod(20,6);
+    printf("2. Yearly Tax Issues");
+
+    getcod(20,7);
+    printf("3. << Go Back");
+
+    getcod(20,10);
+    printf("Select Menu:     ");
+    scanf("%i", &select);
+
+        switch(select){
+        case 1:
+            admin_add_employee_bank_and_tax_details();
+            break;
+        case 2:
+            admin_yearly_tax_issues();
+            break;
+        case 3:
+            admin_assign_facility();
+            break;
+        default:
+            printf("Wrong Input \n");
+            printf("Press any key to Try Again \n");
+            getch();
+            admin_user_menu();
+        }
+
+}
 void admin_fi_salary_administration(){}
-void admin_fi_rewards(){}
-void admin_t_clock_in_or_out(){}
+void admin_add_rewards(){}
+void admin_assign_rewards(){}
+void admin_fi_rewards(){
+system("cls");
+bannerdesign("Rewards");
+    int select;
+
+    getcod(20,5);
+    printf("1. Add Rewards");
+
+    getcod(20,6);
+    printf("2. Assign Rewards");
+
+    getcod(20,7);
+    printf("3. << Go Back");
+
+    getcod(20,10);
+    printf("Select Menu:     ");
+    scanf("%i", &select);
+
+        switch(select){
+        case 1:
+            admin_add_rewards();
+            break;
+        case 2:
+            admin_assign_rewards();
+            break;
+        case 3:
+            Administrator_Menu();
+            break;
+        default:
+            printf("Wrong Input \n");
+            printf("Press any key to Try Again \n");
+            getch();
+            admin_user_menu();
+        }
+
+}
+void admin_t_clock_in_or_out(){
+system("cls");
+bannerdesign("Clock In Or Out");
+    int select;
+
+    getcod(20,5);
+    printf("1. In");
+
+    getcod(20,6);
+    printf("2. Out");
+
+    getcod(20,7);
+    printf("3. << Go Back");
+
+    getcod(20,10);
+    printf("Select Menu:     ");
+    scanf("%i", &select);
+
+        switch(select){
+        case 1:
+            admin_in();
+            break;
+        case 2:
+            admin_out();
+            break;
+        case 3:
+            admin_assign_facility();
+            break;
+        case 4:
+            admin_employee_with_facility();
+            break;
+        case 5:
+            Administrator_Menu();
+            break;
+        default:
+            printf("Wrong Input \n");
+            printf("Press any key to Try Again \n");
+            getch();
+            admin_user_menu();
+        }
+
+}
 void admin_t_time_tracking(){}
 void admin_t_attendance_management(){}
 void admin_p_projects(){}
 void admin_p_client_management(){}
+void admin_add_employee(){}
+void admin_edit_employee(){}
+void admin_delete_employee(){}
+void admin_job_history_overview(){}
+void admin_add_facility(){}
+void admin_view_facility(){}
+void admin_assign_facility(){}
+void admin_employee_with_facility(){}
+void admin_employee_overtime(){}
+void admin_overtime_facility(){}
+void admin_overtime_extra_benefit(){}
+void admin_add_pay_order(){}
+void admin_release_pay_order(){}
+void admin_expense(){}
+void admin_provident_fund(){}
+void admin_health_insurance(){
+system("cls");
+bannerdesign("Health Insurance");
+    int select;
+
+    getcod(20,5);
+    printf("1. Add Insurance Company");
+
+    getcod(20,6);
+    printf("2. Employee With Insurance");
+
+    getcod(20,7);
+    printf("3. Insurance Facility");
+
+    getcod(20,8);
+    printf("4. << Go Back");
+
+    getcod(20,10);
+    printf("Select Menu:     ");
+    scanf("%i", &select);
+
+        switch(select){
+        case 1:
+            admin_add_insurance_company();
+            break;
+        case 2:
+            admin_employee_with_insurance();
+            break;
+        case 3:
+            admin_insurance_facility();
+            break;
+        case 4:
+            Administrator_Menu();
+            break;
+        default:
+            printf("Wrong Input \n");
+            printf("Press any key to Try Again \n");
+            getch();
+            admin_user_menu();
+        }
+
+}
+void admin_add_insurance_company(){}
+void admin_employee_with_insurance(){}
+void admin_insurance_facility(){}
+void admin_life_insurance(){
+system("cls");
+bannerdesign("Life Insurance");
+    int select;
+
+    getcod(20,5);
+    printf("1. Add Insurance Company");
+
+    getcod(20,6);
+    printf("2. Employee With Insurance");
+
+    getcod(20,7);
+    printf("3. Insurance Facility");
+
+    getcod(20,8);
+    printf("4. << Go Back");
+
+    getcod(20,10);
+    printf("Select Menu:     ");
+    scanf("%i", &select);
+
+        switch(select){
+        case 1:
+            admin_add_insurance_company();
+            break;
+        case 2:
+            admin_employee_with_insurance();
+            break;
+        case 3:
+            admin_insurance_facility();
+            break;
+        case 4:
+            Administrator_Menu();
+            break;
+        default:
+            printf("Wrong Input \n");
+            printf("Press any key to Try Again \n");
+            getch();
+            admin_user_menu();
+        }
+
+}
